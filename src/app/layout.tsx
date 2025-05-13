a5d9cbe6d136bf0777fab177247d0c3cb36bca98
Jacob <jakevilevac@gmail.com>
2025-05-12 23:42:46 -0700

    update: theme fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            {children}
-          </div>
+          {children}
         </ThemeProvider>
       </body>
     </html>
