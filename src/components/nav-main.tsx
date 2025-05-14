c7946176515712d8bcbe17a3a045d5dd6c0e4e35
Jacob <jakevilevac@gmail.com>
2025-05-14 00:57:28 -0700

    fix: pathname hydration error on first load, oklch colors in theme

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/sidebar";
 import Link from "next/link";
 
+import { usePathname } from "next/navigation";
+
 export function NavMain({
 	items,
 	category,
@@ -26,20 +28,31 @@ export function NavMain({
 	}[];
 	category: string;
 }) {
+	const pathname = usePathname();
+
 	return (
 		<SidebarGroup>
 			<SidebarGroupLabel>{category}</SidebarGroupLabel>
 			<SidebarMenu>
-				{items.map((item) => (
-					<SidebarMenuItem key={item.title}>
-						<SidebarMenuButton asChild tooltip={item.title}>
-							<Link href={item.url}>
-								{item.icon && <item.icon />}
-								<span>{item.title}</span>
-							</Link>
-						</SidebarMenuButton>
-					</SidebarMenuItem>
-				))}
+				{items.map((item) => {
+					const isActive =
+						// set active, or default to false if no pathnam (first load)
+						// if no pathname then we set only the "/" item to active because its first load
+						pathname === item.url ? true : pathname === "/" && item.url === "/";
+					return (
+						<SidebarMenuItem key={item.title}>
+							<SidebarMenuButton asChild tooltip={item.title} isActive={isActive}>
+								<Link
+									href={item.url}
+									className={isActive ? "font-semibold text-primary" : undefined}
+								>
+									{item.icon && <item.icon />}
+									<span>{item.title}</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					);
+				})}
 			</SidebarMenu>
 		</SidebarGroup>
 	);
