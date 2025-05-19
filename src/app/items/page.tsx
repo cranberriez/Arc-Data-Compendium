38662dff07d6293fdefbaa36b535bf27da0ac415
Jacob <jakevilevac@gmail.com>
2025-05-19 15:23:17 -0700

    fix: temp git cap stupidity

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { ItemCard } from "@/components/items/itemDisplay";
-import { useItems } from "@/contexts/itemContextcap";
+import { ItemCard } from "@/components/items/itemDisplaycap";
+import { useItems } from "@/contexts/ItemContext";
 
 function ItemList() {
 	const { filteredItems } = useItems();
