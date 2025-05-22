7ce8056f56286fc81bf7a35fb339c6cb1cf72cfb
Jacob <jakevilevac@gmail.com>
2025-05-21 20:53:16 -0700

    update: basic recipe types

---
+++ src/data/recipes/recipeData.ts
@@ -0,0 +1,14 @@
+import { Recipe } from "@/types";
+
+export const recipeData: Recipe[] = [
+	{
+		id: "",
+		outputItemId: "",
+		requirements: [],
+		workbench: "none",
+		workbenchTier: 0,
+		craftTime: 0,
+		outputCount: 0,
+		unlockedByDefault: false,
+	},
+];
