11190fbff1b72e1a429f217b40aa22d8040594ff
Jacob <jakevilevac@gmail.com>
2025-05-21 20:24:46 -0700

    fix: recipe exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,18 +3,19 @@
 // Base types and interfaces
 export * from "./items/types";
 export * from "./items/base";
+export * from "./items/workbench";
+export * from "./items/recipe";
 
 // Re-export commonly used types for convenience
 export type { BaseItem } from "./items/base";
 export type { Item } from "./items/item";
 export type { Workbench } from "./items/workbench";
+export type { Recipe, RecipeRequirement } from "./items/recipe";
 
 export type {
 	Rarity,
 	ItemSource,
 	ItemCategory,
-	Recipe,
-	RecipeRequirement,
 	ItemSourceType,
 	QuickUseCategory,
 	ShieldType,
