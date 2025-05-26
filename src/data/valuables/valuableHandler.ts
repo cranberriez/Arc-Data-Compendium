e1d49bf33fbec2334f7f2bc137ac2564bb608158
Jacob <jakevilevac@gmail.com>
2025-05-26 04:26:45 -0700

    update: deleted unused data, handlers, and utils

--- src/data/valuables/valuableHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Item } from "@/types";
-import { valuablesData } from "./valuableData";
-
-// processValuables
-const processedValuables = valuablesData.map((item) => ({
-	...item,
-}));
-
-export const valuables: Item[] = processedValuables;
--- src/data/workbenches/workbenchHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Workbench } from "@/types/items/workbench";
-import { workbenchesData } from "./workbenchData";
-
-// processWorkbenches
-const processedWorkbenches = workbenchesData.map((workbench: Workbench) => ({
-	...workbench,
-}));
-
-export const workbenches: Workbench[] = processedWorkbenches;
