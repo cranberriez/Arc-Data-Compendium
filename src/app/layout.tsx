a41b74797074c026818c2a257edf5abf98448ca8
Jacob <jakevilevac@gmail.com>
2025-05-28 08:43:43 -0700

    fix: accesibility allow user zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export const viewport: Viewport = {
 	width: "device-width",
 	initialScale: 1,
 	maximumScale: 1,
-	userScalable: false,
+	userScalable: true,
 };
 
 export default function RootLayout({
