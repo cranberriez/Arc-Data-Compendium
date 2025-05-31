2990f24975a723cd78c8c9571ef7bad4ad0892b4
Jacob <jakevilevac@gmail.com>
2025-05-31 14:50:39 -0700

    fix: relocated recipe and workshop providers to ecompase entire app

--- src/app/workshop/layout.tsx
+++ src/app/workshop/layout.tsx
@@ -1,10 +1,3 @@
-import { WorkshopProvider } from "@/contexts/workshopContext";
-import { RecipeProvider } from "@/contexts/recipeContext";
-
 export default function WorkshopLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<WorkshopProvider>
-			<RecipeProvider>{children}</RecipeProvider>
-		</WorkshopProvider>
-	);
+	return <>{children}</>;
 }
