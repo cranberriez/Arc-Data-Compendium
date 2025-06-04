2847b79af368e70b5084b68d56e5d74f52a4daea
Jacob <jakevilevac@gmail.com>
2025-06-03 19:53:16 -0700

    delete unused workbench content

--- workbenches/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Workbench } from "@/types/index";
-import { WorkbenchTierContainer } from "./_components/wbTierCard";
-
-export default function WorkbenchesPage({ workbench }: { workbench: Workbench }) {
-	return <WorkbenchTierContainer workbench={workbench} />;
-}
--- workbenches/recipes/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Workbench } from "@/types/index";
-
-export default function WorkbenchesRecipesPage({ workbench }: { workbench: Workbench }) {
-	return <p>Nothing here yet</p>;
-}
