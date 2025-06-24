5e527ebacb9dd4cdc6397cc28c6aae2d023d8e6f
Jacob <jakevilevac@gmail.com>
2025-06-24 07:30:27 -0700

    update: tools links

--- src/components/home/quickLinks.tsx
+++ src/components/home/quickLinks.tsx
@@ -1,12 +1,25 @@
-import { ExternalLink, Wrench } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+	ArrowRight,
+	Calculator,
+	ClipboardCheck,
+	Crosshair,
+	Diff,
+	ExternalLink,
+	LucideIcon,
+	Route,
+	TrendingUpDown,
+	Wrench,
+} from "lucide-react";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 export function QuickLinks() {
 	return (
-		<div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-6">
+		<div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-6">
 			<h2 className="sr-only">Quick Links</h2>
-			<ExternalSiteLinks />
 			<InternalSiteLinks />
+			<ExternalSiteLinks />
 		</div>
 	);
 }
@@ -25,7 +38,7 @@ function ExternalSiteLinks() {
 					description="The official wiki for Arc Raiders, community driven, and updated frequently."
 				/>
 				<ExternalSiteItem
-					name="ARC Raiders Build"
+					name="ARC Raiders Builds"
 					link="https://arcraiders.build"
 					description="A community-driven build site for Arc Raiders with useful guides and more."
 				/>
@@ -73,10 +86,89 @@ function ExternalSiteItem({
 function InternalSiteLinks() {
 	return (
 		<div className="flex flex-col gap-2 bg-card rounded-lg p-6">
-			<h3 className="text-2xl font-semibold mb-4 text-foreground flex items-center gap-4">
-				<Wrench className="w-6 h-6 text-arc-green-600 dark:text-arc-green-400" />
-				Tools
-			</h3>
+			<div className="flex items-center justify-between gap-2 mb-4">
+				<h3 className="text-2xl font-semibold text-foreground flex items-center gap-4">
+					<Wrench className="w-6 h-6 text-arc-green-600 dark:text-arc-green-400" />
+					Tools
+				</h3>
+				<Link href="/tools">
+					<Button
+						size="lg"
+						variant="ghost"
+						className="text-md cursor-pointer"
+					>
+						<p className="mb-1">Explore Tools</p>
+						<ArrowRight />
+					</Button>
+				</Link>
+			</div>
+			<div className="gap-2 grid grid-cols-1 xl:grid-cols-2 flex-1">
+				<ToolLink
+					name="Item Checklists"
+					link="/"
+					icon={ClipboardCheck}
+					iconColor="text-arc-green-600 dark:text-arc-green-400"
+				/>
+				<ToolLink
+					name="Recycling Calculator"
+					link="/"
+					icon={Calculator}
+					iconColor="text-arc-green-600 dark:text-arc-green-400"
+				/>
+
+				<ToolLink
+					name="Weapon Comparisons"
+					link="/"
+					icon={Diff}
+					iconColor="text-red-600 dark:text-red-400"
+				/>
+				<ToolLink
+					name="Weapon Modding"
+					link="/"
+					icon={Crosshair}
+					iconColor="text-red-600 dark:text-red-400"
+				/>
+
+				<ToolLink
+					name="Route Planner"
+					link="/"
+					icon={Route}
+					iconColor="text-blue-600 dark:text-blue-400"
+				/>
+				<ToolLink
+					name="Meta Analysis"
+					link="/"
+					icon={TrendingUpDown}
+					iconColor="text-purple-600 dark:text-purple-400"
+				/>
+			</div>
+			<p className="text-muted-foreground text-sm text-center">
+				Tools are currently in development
+			</p>
 		</div>
+	);
+}
+
+function ToolLink({
+	name,
+	link,
+	icon: Icon,
+	iconColor,
+}: {
+	name: string;
+	link: string;
+	icon: LucideIcon;
+	iconColor?: string;
+}) {
+	return (
+		<Link
+			href={link}
+			className="flex-1"
+		>
+			<div className="h-full flex items-center gap-2 p-4 rounded-sm border-2 border-transparent hover:border-muted-foreground/20 bg-background hover:bg-card transition-colors">
+				<Icon className={cn("w-5 h-5", iconColor)} />
+				<p className="text-foreground flex items-center gap-2 text-lg">{name}</p>
+			</div>
+		</Link>
 	);
 }
