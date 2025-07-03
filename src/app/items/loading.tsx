5e182bdd20287c12e0c1df17066a306423c32a4e
Jacob <jakevilevac@gmail.com>
2025-07-02 20:49:26 -0700

    fix: slight change to fix spaced out items in loader

--- src/app/items/loading.tsx
+++ src/app/items/loading.tsx
@@ -2,7 +2,7 @@ import ItemSkeleton from "@/components/items/itemSkeleton";
 
 export default function itemPageLoader() {
 	return (
-		<main className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-x-6 gap-y-8 min-h-full w-full py-8 px-2 sm:px-4 smooth-scroll">
+		<main className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-x-6 gap-y-8 w-full py-8 px-2 sm:px-4 smooth-scroll">
 			<h1 className="sr-only">All Items</h1>
 			{[...Array(15)].map((_, index) => (
 				<div
